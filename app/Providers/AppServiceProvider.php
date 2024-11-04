06f0b9f5cc43c1df81ee34a4a64e4ccf0fb70a32
Paris Paizal <ppaizal34@gmail.com>
2024-11-04 21:20:02 +0700

    Membuat rate limit untuk API Country dengan permenit 10 request

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('country', function (){
+            return Limit::perMinute(2)->response(function() {
+                return response()->json([
+                    'message' => 'To many request'
+                ], 429);
+            });
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('countries')->group(function(){
+Route::prefix('countries')
+->middleware('throttle:country')
+->group(function(){
     Route::get('random', [CountryController::class, 'random']);
     Route::resource('/', CountryController::class)->parameters(['' => 'country']);
 });
