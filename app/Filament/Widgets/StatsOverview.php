ec176d8989622bbe6956989600bc5eabede572a7
Paris Paizal <ppaizal34@gmail.com>
2024-11-22 21:47:56 +0700

    Menambahkan Stat(total_user, total request api, kunjungan user, pengguna aktif, device) pada widget dashboard

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\User;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+
+class StatsOverview extends BaseWidget
+{
+    protected int | string | array $columnSpan = 'full';
+
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Total User', User::count())
+                ->icon('heroicon-m-user')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('success'),
+            Stat::make('Total Seluruh Request API', User::sum('request_count'))
+                ->icon('heroicon-m-chart-bar')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('success'),
+            Stat::make('Jumlah Kunjugan User Perhari', 250)
+                ->icon('heroicon-m-chart-bar')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('success'),
+            Stat::make('Pengguna Aktif Hari Ini', 100)
+                ->icon('heroicon-m-user-group')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('success'),
+            Stat::make('Device Yang Sering Digunakan Pengguna', 'Desktop')
+                ->icon('heroicon-m-computer-desktop')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('success'),
+        ];
+
+        $this->getColumns();
+    }
+
+    protected function getColumns(): int
+{
+    return 3;
+}
+}
