fc6e0edd9cd5146ecc6705afee63805ba5c1aef3
Paris Paizal <ppaizal34@gmail.com>
2024-11-24 19:44:49 +0700

    Menambahkan ServerMonitoring Page

--- app/Filament/Pages/Stats.php
+++ app/Filament/Pages/Stats.php
@@ -2,18 +2,17 @@
 
 namespace App\Filament\Pages;
 
-use App\Filament\Widgets\StatsOverview;
+use Filament\Pages\Page;
+use App\Filament\Widgets\NewUsers;
 use App\Filament\Widgets\StatsChart;
 use App\Filament\Widgets\LatestUsers;
-use App\Filament\Widgets\GeographicalUser;
-use App\Filament\Widgets\NewUsers;
 
-use Filament\Pages\Page;
+use App\Filament\Widgets\StatsOverview;
+use App\Filament\Widgets\GeographicalUser;
 
 class Stats extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
-
     protected static string $view = 'filament.pages.stats';
 
     protected function getHeaderWidgets(): array
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers\Filament;
 
-use App\Filament\Pages\Dashboard;
+use App\Filament\Pages\ServerMonitoring;
+use Filament\Pages;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -33,7 +34,8 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-                Dashboard::class,
+                Pages\Dashboard::class,
+                ServerMonitoring::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
